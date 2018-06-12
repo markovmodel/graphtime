bf5ad53e29f48bcb610b369dda2766ab91e8eec3
Simon Olsson <simols@hotmail.com>
2018-06-12 16:52:19 +0200

    minor clean up still not functional

--- graphtime/ising_utils.py
+++ graphtime/ising_utils.py
@@ -13,29 +13,29 @@ def Ising_tmatrix(nspins, alpha=0.1, gamma=0.95, ratematrix=False):
         gamma: gamma is equal to tanh(\beta 2J) where J is the spin-spin coupling constant in a corresponding Ising model, and \beta is the inverse temperature. 
         ratematrix: return rate matrix as well
     """
-    W = _np.zeros((2**nspins, 2**nspins))
-    for i, s in enumerate(_itrt.product([-1, 1], repeat = nspins)):
-        s = _np.array(s)
-        for j, c in enumerate(_itrt.product([-1, 1], repeat = nspins)):
-            c = _np.array(c)
-            if _np.all(s == c):
-                #Diagonal is filled later.
-                continue
-            else:
-                flipped = np.where(s!=c)[0]
-                if len(flipped)==1:
-                    f = flipped[0]
-                    W[i, j] = 0.5*alpha*(1.-0.5*gamma*s[f]*(s[f-1]+s[(f+1)%nspins]))
-                else:
-                    pass
-    #fill diagonal
-    W[_np.diag_indices(2**nspins)] = -W.sum(axis=1)
-    #compute transition matrix
-    T = _expm(W)
-    if ratematrix:
-        return T, W
-    else:
-        return T
+  W = _np.zeros((2**nspins, 2**nspins))
+  for i, s in enumerate(_itrt.product([-1, 1], repeat = nspins)):
+    s = _np.array(s)
+    for j, c in enumerate(_itrt.product([-1, 1], repeat = nspins)):
+      c = _np.array(c)
+      if _np.all(s == c):
+        #Diagonal is filled later.
+        continue
+      else:
+        flipped = np.where(s!=c)[0]
+        if len(flipped)==1:
+          f = flipped[0]
+          W[i, j] = 0.5*alpha*(1.-0.5*gamma*s[f]*(s[f-1]+s[(f+1)%nspins]))
+        else:
+          pass
+  #fill diagonal
+  W[_np.diag_indices(2**nspins)] = -W.sum(axis=1)
+  #compute transition matrix
+  T = _expm(W)
+  if ratematrix:
+    return T, W
+  else:
+    return T
 
 def Ising_to_discrete_state(X):
   """
@@ -47,7 +47,7 @@ def Ising_to_discrete_state(X):
 		returns:
 		dts : list of lists ( discrete state trajectories)
 	"""
-	dts = []
+  dts = []
   for x in X:
     x[np.where(x==-1)] = 0
     dts.append ([int(''.join(map(str, f)), 2) for f in x])
--- graphtime/markov_random_fields.py
+++ graphtime/markov_random_fields.py
@@ -1,53 +1,41 @@
-from _base import Model as _Model
-import numpy as _np
 
-class MRF(_Model):
-	def __init__(self, couplings, fields):
-		assert(isinstance(list, couplings))
-		assert(isinstance(list, fields))
-		assert(len(couplings) == len(fields))
-		
+import itertools as _itrt
+import numpy as _np
+class MRF(object):
+  def __init__(self, couplings, fields):
+    assert(isinstance(list, couplings))
+    assert(isinstance(list, fields))
+    assert(len(couplings) == len(fields))
+
     self.nsubsys = len(fields)
-		self.couplings = couplings
-		self.fields = fields
+    self.couplings = couplings
+    self.fields = fields
 
   def simulate(self, nsteps, start = None):
-			if not isinstance(start, np.ndarray):
-					_s = 2*_np.random.binomial(1, 0.5, size = )-1
-			else:
-					_s = 2*start.copy()-1
-			_states = _np.zeros((h.shape[0], n_steps))
-			_states[:, 0] = _s.copy()
-			
-			rnd = _np.random.rand((n_steps-1)*h.shape[0]).reshape((h.shape[0], n_steps - 1))
-			for i in range(n_steps-1):
-					tprob = 1./(1.+np.exp(-_J.dot(_s)-h))
-					_s = 2*(rnd[:, i] < tprob).astype(int)-1
-					_states[:, i + 1] = _s.copy()
-			return _states.T
+    """ assumes everything is binary """
+    if not isinstance(start, np.ndarray):
+       _s = 2*_np.random.binomial(1, 0.5, size = )-1
+    else:
+       _s = 2*start.copy()-1
+    _states = _np.zeros((self.nsubsys, nsteps))
+    _states[:, 0] = _s.copy()
 
-
-class BinaryMRF(_Model, MRF):
+    rnd = _np.random.rand((n_steps-1)*self.nsubsys]).reshape((self.nsubsys, nsteps - 1))
+    for i in range(nsteps-1):
+      tprob = 1./(1.+np.exp(-self.couplings.dot(_s)-self.fields))
+      _s = 2*(rnd[:, i] < tprob).astype(int)-1
+      _states[:, i + 1] = _s.copy()
+   
+   return _states.T
 
   def generate_transition_matrix(self, force = True):
-    nspins=7
-    T = np.zeros((2**nspins, 2**nspins))
-    for i,s_ in enumerate(product([-1,1], repeat=nspins)):
-        s = np.array(s_)
-        if force_reversible:
-            tprob = 1./(1.+np.exp(-Jp.dot(s)-h))
-
-        else:
-            
-            tprob = 1./(1.+np.exp(-J.dot(s)-h))
-        for j,z_ in enumerate(product([-1,1], repeat=nspins)):
-            z = np.array(z_)
-            T[i, j] = np.prod(tprob[z==1])*np.prod(1-tprob[z==-1])
+    T = np.zeros((2**self.nsubsys, 2**self.numsubsys))
+    for i,s_ in enumerate(_itrt.product([-1,1], repeat = nspins)):
+      s = np.array(s_)
+      tprob = 1./(1.+np.exp(-self.couplings.dot(s)-self.fields))
+      for j,z_ in enumerate(product([-1,1], repeat=nspins)):
+        z = np.array(z_)
+        T[i, j] = np.prod(tprob[z==1])*np.prod(1-tprob[z==-1])
     return T
 
 
-class GeneralMRF(_Model, MRF):
-  def simulate(self, nsteps, start = None):
-
-  def generate_transition_matrix(self):
-  
