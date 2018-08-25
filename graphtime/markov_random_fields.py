b7e1b25cc5dd63a58daed57310710846a438edca
Simon Olsson <simols@hotmail.com>
2018-08-25 12:36:43 +0200

    revamp

--- graphtime/markov_random_fields.py
+++ graphtime/markov_random_fields.py
@@ -3,99 +3,112 @@ import numpy as _np
 from sklearn.preprocessing import OneHotEncoder as _OneHotEncoder
 from sklearn.linear_model import LogisticRegression as _LogisticRegression
 
+
 class dMRF(object):
-  """
-		Implements a dynamic Markov random field model as described in Olsson and Noe 2018
-	"""
+    """
+        Implements a dynamic Markov random field model as described in Olsson and Noe 2018
+    """
 
-	def __init__(self, couplings, fields, active_subsystems, estimated = False):
-    assert(isinstance(list, couplings))
-    assert(isinstance(list, fields))
-    assert(len(couplings) == len(fields))
+    def __init__(self, lrs, active_subsystems, enc = None, estimated = False):
+        """
+            lrs :  list of LogisticRegression instances (sklearn)
+            active_subsystems : list of active sub-systems
+            estimated : bool, indicator whether dMRF is estimated
+        """
+        
+        self.nsubsys_ = len(lrs)
+        self.lrs = lrs
+        self.encoder = enc
+        self.active_subsystems_ = active_subsystems
+        self.estimated_ = estimated
 
-    self.nsubsys_ = len(fields)
-    self.couplings_ = couplings
-    self.fields_ = fields
-		self.active_subsystems_ = active_subsystems 
-		self.estimated_ = estimated 
+    def simulate(self, nsteps, start = None):
+        # if there is no initial condition generate one
+        if not isinstance(start, _np.ndarray): 
+            _s = _np.array([lr.classes_[_np.random.randint(0, len(lr.classes_))] for lr in self.lrs])
+        
+        #pre-generate random numbers gives very slight speedups for large nsteps
+        rnd = _np.random.rand((n_steps-1)*self.nsubsys).reshape((self.nsubsys, nsteps - 1))
+        
+        _states = _np.zeros((self.nsubsys_, nsteps))
+        _states[:, 0] = _s.copy()
 
-  def simulate(self, nsteps, start = None):
-    """ assumes everything is binary """
-    if not isinstance(start, np.ndarray):
-       _s = 2*_np.random.binomial(1, 0.5, size = )-1
-    else:
-       _s = 2*start.copy()-1
-    _states = _np.zeros((self.nsubsys, nsteps))
-    _states[:, 0] = _s.copy()
+        for n in range(nsteps-1):
+            new_state = []
+            #for every sub-system sample new configuration given current global configuration
+            for j,lr in enumerate(self.lrs):
+                cmf = _np.cumsum(lr.predict_proba(self.encoder.transform([_states[:, n]])))
+                _states[j, n + 1] = _np.searchsorted(cmf, rnd[j, n])
 
-    rnd = _np.random.rand((n_steps-1)*self.nsubsys]).reshape((self.nsubsys, nsteps - 1))
-    for i in range(nsteps-1):
-      tprob = 1./(1.+np.exp(-self.couplings.dot(_s)-self.fields))
-      _s = 2*(rnd[:, i] < tprob).astype(int)-1
-      _states[:, i + 1] = _s.copy()
-   
-   return _states.T
+        return _states.T
 
-  def generate_transition_matrix(self, force = True):
-    T = np.zeros((2**self.nsubsys, 2**self.numsubsys))
-    for i,s_ in enumerate(_itrt.product([-1,1], repeat = nspins)):
-      s = np.array(s_)
-      tprob = 1./(1.+np.exp(-self.couplings.dot(s)-self.fields))
-      for j,z_ in enumerate(product([-1,1], repeat=nspins)):
-        z = np.array(z_)
-        T[i, j] = np.prod(tprob[z==1])*np.prod(1-tprob[z==-1])
-    return T
+    def generate_transition_matrix(self, safemode = True, maxdim = 10000):
+        ndims = _np.prod([len(lr.classes_) for lr in lrs])
+        if ndims > maxdim and safemode:
+            raise MemoryError(
+                'Maximum safe-mode transition matrix dimension ({:i}x{:i}) exceeded.'.format(maxdim, maxdim))
+        idx_ = [list(range(len(lr.classes_))) for lr in lrs]
 
-def estimate_potts(strajs, lag = 1, stride = 1, fit_bias = False, regularizer = 'l1', C = 1., tol=1e-4):
+        T = _np.zeros((ndims, ndims))
+        
+        for i, s in enumerate(product(*[lr.classes_ for lr in self.lrs])):
+            _se = self.encoder.transform(np.array([s]))
+            # compute transition probabilities for each sub-system state at time t+\tau
+            tprobs = [lr.predict_proba(_se) for lr in self.lrs]
+            for j, z_ in enumerate(product(*[lr.classes_ for lr in self.lrs])):
+                # compute product of outcome state
+                T[i, j] = _np.prod([tprob[:, idx.index(z)] for idx, tprob, z in zip(idx_, tprobs, list(z_))])
+        return T
+    
+    
+def estimate_dMRF(strajs, lag = 1, stride = 1, Encoder = _OneHotEncoder(sparse=False), 
+                  logistic_regression_kwargs = {'fit_intercept': False, 
+                   'penalty': 'l1', 'C': 1., 'tol': 1e-4, 'solver': 'saga'}):
     """
         Arguments:
         ----------------------
         strajs (list of ndarrays): state of each subsystem as a function of time.
         lag (int=1): lag-time used in auto-regression
         stride (int=1): data stride prior to model estimation. lag should be devisible by this quantity.
-        fit_fields (bool=False): flag indicating whether fields/biases (h) are estimated.
-        regularizer (str='l1'): flag for regularization scheme ('l1': Laplacian prior, 'l2': Gaussian prior )
-        C (float=1.): factor inversely proportional to regularization strength.
+        logistic_regression_kwargs (dict): dictionary of keyword arguments forwarded to 
+            sklearn LogisticRegression. 
+            The multi_class kwargs is forced to 'ovr' for binary cases and 'multinomial' for multinomial cases.
         
         returns:
-					dMRF : an estimated dynamic Markov Random Field	
+            params_potts (list): coupling parameters for each sub-system
+            intercepts_potts (list): intercept parameters for each sub-system
+            Encoder: OneHotEncoder to used to featurize
+            active_subsystems: indices of non constant subsystems
     """
-    Encoder = _OneHotEncoder()
+    Encoder = Encoder
 
     strided_strajs = [t[::stride] for t in strajs]
-    P0 = np.vstack([t[:-lag//stride] for t in strided_strajs])
-    Pt = np.vstack([t[lag//stride:] for t in strided_strajs]) 
+    P0 = _np.vstack([t[:-lag//stride] for t in strided_strajs])
+    Pt = _np.vstack([t[lag//stride:] for t in strided_strajs]) 
     nframes_strided, nsubsys = P0.shape
-    active_subsystems_0 = np.where([len(np.unique(P0[:, i]))>1 for i in range(nsubsys)])[0]
-    active_subsystems_t = np.where([len(np.unique(P0[:, i]))>1 for i in range(nsubsys)])[0]
+    
+    #find active sub-systems
+    active_subsystems_0 = _np.where([len(np.unique(P0[:, i]))>1 for i in range(nsubsys)])[0]
+    active_subsystems_t = _np.where([len(np.unique(Pt[:, i]))>1 for i in range(nsubsys)])[0]
     active_subsystems = list(set(active_subsystems_0).intersection(active_subsystems_t))
-
+    lrs= []
+    
     #remove constant spins
     P0 = P0[:, active_subsystems]
     Pt = Pt[:, active_subsystems]
     
-    #encode multiple
-    if len(np.unique(P0))>2:
-        P0 = Encoder.fit_transform(P0)
-    
-    params_potts=[]
-    intercepts_potts=[]
+    P0 = Encoder.fit_transform(P0)
     
     for i in range(Pt.shape[1]):
         # if only two categories use one-versus-rest estimation mode
-        mc = 'ovr'
+        logistic_regression_kwargs['multi_class'] = 'ovr'
         
-        if len(np.unique(Pt[:, i]))>2:
+        if len(_np.unique(Pt[:, i]))>2:
             # if we have more than 2 states change multiclass flag to multinomial
-            mc = 'multinomial'
-        logr = _LogisticRegression(fit_intercept = fit_bias, C = C, penalty = regularizer, 
-                                    solver = 'saga', tol=tol, multi_class=mc).fit(P0, Pt[:, i])
+            logistic_regression_kwargs['multi_class'] = 'multinomial'
+        logr = _LogisticRegression(**logistic_regression_kwargs).fit(P0, Pt[:, i])
+        lrs.append(logr)
 
+    
+    return dMRF(lrs, active_subsystems, enc = Encoder, estimated = True)
 
-        params_potts.append(logr.coef_.copy())
-        intercepts_potts.append(logr.intercept_)
-
-    return params_potts, intercepts_potts, Encoder, active_subsystems
-
-
-#def estimate_dynamic_MRF()
